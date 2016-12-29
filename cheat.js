b917d9aab4c328ca139c77329742fa653be54809
benko <benko@3cps.ca>
2016-12-29 15:17:21 +0800

    skip vaild url

--- cheat.js
+++ cheat.js
@@ -19,6 +19,11 @@ app.use(express.static(__dirname + '/public'));
 
 var urls = ['http://112.74.141.140/amuc/api/activity/getActivityList']
 
+function validateURL(url){
+  var urlregex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[\/?#]\S*)?$/i;
+  return urlregex.test(url);
+}
+
 app.get("/command", function(req, res){
   if(!!!req.query.namespace){
     res.jsonp({error: 1, msg: "params erorr"})
@@ -118,6 +123,10 @@ app.use("*", function(req, res, next) {
     originalUrl = req.headers['origin-uri']
   }
   console.log("target:" + target)
+  if(!validateURL(target)){
+    res.json({})
+    return
+  }
   if (req.method == 'POST' || req.method == 'PUT') {
     var options = {
       headers: req.headers,
